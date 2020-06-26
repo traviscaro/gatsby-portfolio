57a19f5933bdaca7d018a19b08085dfa02bf3dee
Travis Caro <travisbcaro@gmail.com>
2020-06-25 23:42:34 -0400

    Add simple components and global stylesheet

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,17 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Header from "../components/header"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <Header siteTitle="Travis's Gatsby"/>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
+    <Link to="/about">About</Link> <br />
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
