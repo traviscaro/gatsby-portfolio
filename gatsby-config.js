21f7090b42b34f9fe07c04e0e34e747d49ad0f6a
Travis Caro <travisbcaro@gmail.com>
2020-06-26 20:48:59 -0400

    Update title, description, and author

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Travis Caro | Software Engineer`,
+    description: `Travis Caro's personal website and portfolio`,
+    author: `Travis Caro`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
