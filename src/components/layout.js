b5a9b982ecca9f4a4d9ba848129c452c9a5fdd1d
Travis Caro <travisbcaro@gmail.com>
2020-08-20 22:38:04 -0400

    Add font awesome and social media icons

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,6 +12,15 @@ import Header from "./header"
 import Footer from "./footer"
 import "./layout.css"
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/pro-regular-svg-icons'
+import { fal } from '@fortawesome/pro-light-svg-icons'
+import { fad } from '@fortawesome/pro-duotone-svg-icons'
+
+library.add(fab, fas, far, fal, fad)
+
 export default ({ children }) => {
   // const data = useStaticQuery(graphql`
   //   query SiteTitleQuery {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,39 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    
-    <p className="intro">
-      👋 Hey, I'm Travis Caro!
-    </p>
+
+    <p className="intro">👋 Hey, I'm Travis Caro!</p>
 
     <p style={{ width: "400px", margin: "auto" }}>
-      My site is in development right now, but please check back in from time to time. I'm actively developing it in my freetime.
-
-      <br/>
-      <br/>
-
-      In the meantime, feel free to say <a href="mailto:hello@traviscaro.dev">hello@traviscaro.dev</a>
+      My site is in development right now, but please check back in from time to
+      time. I'm actively developing it in my freetime.
+      <br />
+      <br />
+      In the meantime, feel free to say{" "}
+      <a href="mailto:hello@traviscaro.dev">hello@traviscaro.dev</a>
     </p>
-    
+
+    <div className="social-media-icon-container">
+      <a href="https://twitter.com/travisbcaro" className="social-media-link">
+        <FontAwesomeIcon icon={["fab", "twitter"]} />
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/travisbcaro/"
+        className="social-media-link"
+      >
+        <FontAwesomeIcon icon={["fab", "linkedin-in"]} />
+      </a>
+
+      <a href="https://github.com/tbcaro" className="social-media-link">
+        <FontAwesomeIcon icon={["fab", "github"]} />
+      </a>
+    </div>
   </Layout>
 )
 
