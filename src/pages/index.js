6ec8a93f58a369c4f62532799fb1b53f8fdb1516
Travis Caro <travisbcaro@gmail.com>
2020-09-01 09:32:42 -0400

    Add logo and content

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,23 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
 
-    {/* Add logo image to site */}
-    {/* Then add the logo as the favicon */}
-    {/* <img src="../../images/tc-logo.png" /> */}
+    <div className="logo">
+      <Img fixed={data.file.childImageSharp.fixed} />
+    </div>
 
-    <p className="intro">👋 Hey, I'm Travis!</p>
-    <p></p>
+    <p className="intro">
+      👋 Hey, I'm Travis!
+    </p>
+    <p className="summary">
+      I'm a software engineer, freelance web developer, and adventurer.
+    </p>
 
     <div className="btn-contact-container">
       <a className="btn-contact" href="mailto:hello@traviscaro.dev">Say Hello</a>
@@ -20,3 +27,14 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "tc-logo.png" }) {
+      childImageSharp {
+        fixed(width: 125, height: 125) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
